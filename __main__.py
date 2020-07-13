9197fdc4b5bd5f03b7dda25ed63dd34993bc5832
Ayush Suresh <byteslayer31037@gmail.com>
2020-07-13 01:48:48 -0500

    program prints out the messages being typed in chat

--- __main__.py
+++ __main__.py
@@ -2,13 +2,15 @@ import json
 import socket
 
 with open("consts.json") as f:
-	json_data = json.load(f)
+    json_data = json.load(f)
+
+readbuffer = ""
 
 server = json_data['server']
 port = json_data['port']
 token = json_data['token']
 channel = json_data['channel']
-nickname = 'ayushboss23'
+nickname = 'chatchesspollbot'
 
 sock = socket.socket();
 sock.connect((server,port))
@@ -17,8 +19,42 @@ sock.send(f"PASS {token}\n".encode('utf-8'))
 sock.send(f"NICK {nickname}\n".encode('utf-8'))
 sock.send(f"JOIN {channel}\n".encode('utf-8'))
 
-resp = sock.recv(2048).decode('utf-8')
+def sendMessage(message):
+    s.send(f"PRIVMSG {channel} :" + message + "\r\n")
 
-resp = sock.recv(2048).decode('utf-8')
+while True:
+    readbuffer = readbuffer + str(sock.recv(1024))
+    temp = str.split(readbuffer, "\\n")
+    readbuffer = temp.pop()
 
-print(resp)
+    for line in temp:
+        if (line[0] == "PING"):
+            s.send("PONG %s\r\n" % line[1])
+        else:
+            parts = str.split(line, ":")
+ 
+            if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
+                try:
+                    message = parts[2][:len(parts[2]) - 1]
+                except:
+                    message = ""
+
+                usernamesplit = str.split(parts[1], "!")
+                username = usernamesplit[0]
+
+                MODT = False
+
+                for l in parts:
+                    if "End of /NAMES list" in l:
+                        MODT = True
+               
+                message = message[:-1]
+
+                print("Message: " + str(message))
+                if MODT:
+                    print(username + ": " + message)
+                   
+                    # You can add all your plain commands here
+                    if message == "Hey":
+                        sendMessage("Welcome to my stream, " + username)
+ 
